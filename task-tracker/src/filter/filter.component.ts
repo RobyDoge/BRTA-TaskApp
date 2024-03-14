9fbf7f33b0a1e7db9e70414ad2c690110e8b8b6c
RobyDoge <robyionut23@gmail.com>
2024-03-14 23:37:12 +0200

    Add filter buttons to filter component

--- task-tracker/src/filter/filter.component.ts
+++ task-tracker/src/filter/filter.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Status } from '../status';
+import { CommonModule } from '@angular/common';
+import { MatButton } from '@angular/material/button';
 @Component({
   selector: 'app-filter',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,MatButton],
   templateUrl: './filter.component.html',
   styleUrl: './filter.component.scss'
 })
-export class FilterComponent {
-
-}
+export class FilterComponent implements OnInit {
+  ngOnInit(): void {
+    // Implement initialization logic here
+    console.log('FilterComponent initialized');
+  }	
+  statuses = Object.values(Status);
+} 
+  
