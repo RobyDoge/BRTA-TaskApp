069ac4e0c195304e1db459e2007380e77b55ceb2
RobyDoge <robyionut23@gmail.com>
2024-03-14 21:07:05 +0200

    Add toggle buttons for grid and list view

--- task-tracker/src/tasks-view/tasks-view.component.ts
+++ task-tracker/src/tasks-view/tasks-view.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { TaskGridComponent } from '../task-grid/task-grid.component';
 import { Task } from '../task';
 import { Status } from '../status';
-
+import { TaskListComponent } from '../task-list/task-list.component';
+import { MatIcon } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-tasks-view',
   standalone: true,
-  imports: [TaskGridComponent],
+  imports: [TaskGridComponent, TaskListComponent,MatIcon,CommonModule],
   templateUrl: './tasks-view.component.html',
   styleUrl: './tasks-view.component.scss'
 })
@@ -23,4 +25,6 @@ export class TasksViewComponent {
     title:"Task1 title",
     status:Status.Done
   }         ]
+
+  isList:boolean=true; 
 }
