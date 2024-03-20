9821a9b58a888667df17c027dc7d32368668709c
RobyDoge <robyionut23@gmail.com>
2024-03-20 19:27:56 +0200

    Add status filtering functionality to task list

--- task-tracker/src/filter/filter.component.ts
+++ task-tracker/src/filter/filter.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit,Output } from '@angular/core';
 import { Status } from '../status';
 import { CommonModule } from '@angular/common';
 import { MatButton } from '@angular/material/button';
+
 @Component({
   selector: 'app-filter',
   standalone: true,
@@ -11,9 +12,13 @@ import { MatButton } from '@angular/material/button';
 })
 export class FilterComponent implements OnInit {
   ngOnInit(): void {
-    // Implement initialization logic here
-    console.log('FilterComponent initialized');
   }	
   statuses = Object.values(Status);
+  
+  selectStatus(status: Status): void {
+    this.statusSelected.emit(status)
+  }
+
+  @Output() statusSelected: EventEmitter<Status> = new EventEmitter();
 } 
   
--- task-tracker/src/task-list/task-list.component.ts
+++ task-tracker/src/task-list/task-list.component.ts
@@ -1,14 +1,23 @@
 import { Component, Input } from '@angular/core';
 import { Task } from '../task';
 import { CommonModule } from '@angular/common';
+import { FilterComponent } from '../filter/filter.component';
+import { Status } from '../status';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FilterComponent],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss'
 })
 export class TaskListComponent {
-  @Input() tasks: Task[];
+handleStatusSelected($event: Status) {
+  this.filtredTasks = 
+    this.tasks.filter((task) => task.status === $event);
+
 }
+  @Input() tasks: Task[];
+  @Input() filtredTasks: Task[];
+}
+
