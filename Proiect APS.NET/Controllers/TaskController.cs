37869ed9f1eda03e8cc8607b9b7eb0c6ddcb7044
RobyDoge <robyionut23@gmail.com>
2024-04-17 14:42:39 +0300

    Lab 8 - Done

--- Proiect APS.NET/Controllers/TaskController.cs
+++ Proiect APS.NET/Controllers/TaskController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Proiect_APS.NET.Models;
 
 namespace Proiect_APS.NET.Controllers
 {
@@ -6,30 +7,88 @@ namespace Proiect_APS.NET.Controllers
     [Route("[controller]")]
     public class TaskController: ControllerBase
     {
-        [HttpGet(Name = "GetTasks")]
-        public ObjectResult Get()
-        {
-            return Ok("This is a list of tasks");
-        }
+        private static List<TaskModel> Tasks { get; } =
+        [
+            new TaskModel
+            {
+                Id = Guid.NewGuid(), Title = "First Task", Description = "First Task Description",
+                AssignedTo = "Author_1",
+                Status = "To do"
+            },
+            new TaskModel
+            {
+                Id = Guid.NewGuid(), /*CategoryId = "1",*/ Title = "Second Task",
+                Description = "Second Task Description",
+                AssignedTo = "Author_1", Status = "To do"
+            },
+            new TaskModel
+            {
+                Id = Guid.NewGuid(), /*CategoryId = "1",*/ Title = "Third Task", Description = "Third Task Description",
+                AssignedTo = "Author_2", Status = "To do"
+            },
+            new TaskModel
+            {
+                Id = Guid.NewGuid(), /*CategoryId = "1",*/ Title = "Fourth Task",
+                Description = "Fourth Task Description",
+                AssignedTo = "Author_3", Status = "To do"
+            },
+            new TaskModel
+            {
+                Id = Guid.NewGuid(), /*CategoryId = "1",*/ Title = "Fifth Task", Description = "Fifth Task Description",
+                AssignedTo = "Author_4", Status = "To do"
+            }
+        ];
 
-        [HttpDelete(Name = "DeleteTask")]
-        public ObjectResult Delete()
+        [HttpGet(Name = "GetTasks")]
+        public IActionResult GetTasks()
         {
-            return Ok("Task deleted");
+            return Ok(Tasks);
         }
 
         [HttpPost(Name = "CreateTask")]
-        public ObjectResult Post()
+        public IActionResult CreateTask([FromBody] TaskModel? task )
         {
-            return Ok("Task created");
+            if (task == null)
+            {
+                return BadRequest("Task cannot be null");
+            }
+            return Ok(task);
         }
 
         [HttpPut(Name = "UpdateTask")]
-        public ObjectResult Put()
+        public IActionResult Put([FromBody] TaskModel? task)
         {
-            return Ok("Task updated");
+            if (task == null) return BadRequest("Task cannot be null");
+            var found = false;
+            foreach (var t in Tasks.Where(t => t.Id == task.Id))
+            {
+                found = true;
+                t.Title = task.Title;
+                t.Description = task.Description;
+                t.AssignedTo = task.AssignedTo;
+                t.Status = task.Status;
+            }
+            if(!found) return NotFound(404);
+
+            return StatusCode(200);
         }
 
+        [HttpDelete(Name = "DeleteTask")]
+        public IActionResult Delete([FromBody] Guid? taskId)
+        {
+            if (taskId == null) return BadRequest("Task id cannot be null");
+            var found = false;
+            foreach (var t in Tasks.Where(t => t.Id == taskId))
+            {
+                found = true;
+                Tasks.Remove(t);
+                break;
+            }
+            if (!found) return NotFound(404);
+
+            return StatusCode(200);
+
+        }
 
 
 
---
+++ Proiect APS.NET/Models/TaskModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Proiect_APS.NET.Models;
+
+public class TaskModel
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string AssignedTo { get; set; }
+    public string Status { get; set; }
+
+
+
+}
